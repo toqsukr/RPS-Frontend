534c6a03ec1a5f34a066d79af957de3d4577a28b
Wastard <Ilya.pakylov@gmail.com>
2024-01-06 19:57:53 +0300

    added swipe animation

--- src/components/imageBox/ImageBox.component.tsx
+++ src/components/imageBox/ImageBox.component.tsx
@@ -1,28 +1,44 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import css from './ImageBox.module.scss'
 import { IImageBox } from './ImageBox.interface'
 import AuthorBar from './authorBar/AuthorBar.component'
 import { useSwipeable } from 'react-swipeable'
+import classNames from 'classnames'
 
 const ImageBox: FC<IImageBox> = ({ ...props }) => {
   const { currentImageID, images, setCurrentImageID } = props
+  const [swipeDirection, setSwipeDirection] = useState<'Right' | 'Left' | null>(null)
   const handlers = useSwipeable({
     onSwipedLeft: () => {
-      setCurrentImageID((prevImage: number) =>
-        prevImage === images.length - 1 ? 0 : prevImage + 1
-      )
+      setSwipeDirection('Left')
+      setTimeout(() => {
+        setSwipeDirection(null)
+        setCurrentImageID((prevImage: number) =>
+          prevImage === images.length - 1 ? 0 : prevImage + 1
+        )
+      }, 400)
     },
     onSwipedRight: () => {
-      setCurrentImageID((prevImage: number) =>
-        prevImage === 0 ? images.length - 1 : prevImage - 1
-      )
+      setSwipeDirection('Right')
+      setTimeout(() => {
+        setSwipeDirection(null)
+        setCurrentImageID((prevImage: number) =>
+          prevImage === 0 ? images.length - 1 : prevImage - 1
+        )
+      }, 400)
     },
     delta: 100,
     preventScrollOnSwipe: true,
     trackMouse: true,
   })
   return (
-    <div {...handlers} id={css.home_image_box_container}>
+    <div
+      className={classNames({
+        [css.swipe_right]: swipeDirection == 'Right',
+        [css.swipe_left]: swipeDirection == 'Left',
+      })}
+      {...handlers}
+      id={css.home_image_box_container}>
       <AuthorBar {...images[currentImageID].author} />
     </div>
   )
