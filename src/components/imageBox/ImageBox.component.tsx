e5a28c94230597b3047d2b9dd997311bb5699ebd
Wastard <Ilya.pakylov@gmail.com>
2024-01-06 12:07:11 +0300

    deleted footer; added imagebox; set containers structure

---
+++ src/components/imageBox/ImageBox.component.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react'
+import css from './ImageBox.module.scss'
+
+const ImageBox: FC = () => {
+  return (
+    <div id={css.home_image_box_container}>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio enim delectus accusantium
+      minima error aspernatur? Voluptatum sequi qui veritatis mollitia quo magnam tempore, atque
+      autem hic in, harum odit ad.
+    </div>
+  )
+}
+
+export default ImageBox
